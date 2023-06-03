248f9321ceea8262cdfc6a1ce5d9537019e4ed70
Larissa <larissainesdemorais@hotmail.com>
2023-06-03 20:48:10 -0300

    Exercicio de teste 1 - sem correção

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -5,9 +5,35 @@
 // check out the link below and learn how to write your first test:
 // https://on.cypress.io/writing-first-test
 
+//<reference types="Cypress" />
 describe('Central e Atendimento ao Cliente TAT', function() {
-    it('verifica o titulo da aplicação', function(){
+    beforeEach(()=> {
         cy.visit('./src/index.html')
+    })
+
+    it('verifica o titulo da aplicação', function(){
         cy.title().should('be.equal','Central de Atendimento ao Cliente TAT')
+    })
+
+    it('preenche os campos obrigatórios e envia o formulário', function(){
+        cy.get('input[id="firstName"]')
+            .type('Fulano')
+
+        cy.get('input[id="lastName"]')
+            .type('da Silva')
+        
+        cy.get('input[id="email"]')
+            .type('Fulano@gmail.com')
+        
+        cy.get('textarea[id="open-text-area"]')
+            .type('Qual é o sentida da vida')
+
+        cy.get('button[class="button"]')
+            .click()
+
+        cy.get('class="success"')
+            .should('be.visible')
+            .should('be.equal','Mensagem enviada com sucesso.')
+        
     })
 })
